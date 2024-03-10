47c0094e3869b0c51d8235bf870b7cd40a4138c7
Aaron Tran <aarrron.tran@gmail.com>
2024-03-10 21:44:46 +1100

    Changed Procfile

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5432)
+    port = int(os.environ.get("PORT", 33507))
+    print(port)
+    app.run(host="0.0.0.0", port=port)
