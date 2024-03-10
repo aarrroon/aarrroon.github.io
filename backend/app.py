a240ab9c31a504dcbaf24207ff5cd5533f057400
Aaron Tran <aarrron.tran@gmail.com>
2024-03-10 21:45:46 +1100

    Changed Procfile

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,6 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 33507))
+    port = int(os.getenv('PORT'))
     print(port)
     app.run(host="0.0.0.0", port=port)
