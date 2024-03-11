c2b4935ffb3d720943de47f630b851e07b12163a
Aaron Tran <aarrron.tran@gmail.com>
2024-03-11 16:20:50 +1100

    Added a more realistic test data

--- backend/app.py
+++ backend/app.py
@@ -10,33 +10,21 @@ POSTGRES_URL = "postgres://uf8be187h4nlk3:pd6b97fef32d82edc18a09f693e71b14979a77
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-@app.route('/test_data')
-def get_test_data():
-    # Connect to the database
-    # Execute SQL query to select test data
-    connection, cursor = connect_postgres(POSTGRES_URL)
-
-    cursor.execute("SELECT * FROM POI")
-
-    # Fetch all rows
-    rows = cursor.fetchall()
-
-    # Close cursor and connection
-    cursor.close()
-    connection.close()
-
-    result = [{'id': row[0], 'name': row[1]} for row in rows]
-    print(result)
-    # Return JSON response
-    return jsonify(result)
-
-
 # Establish Data Connection
 def connect_postgres(url):
+    """
+    Connects to a PostgreSQL database using the provided URL.
+
+    Args:
+        url (str): The URL of the PostgreSQL database.
+
+    Returns:
+        tuple: A tuple containing the connection and cursor objects.
+
+    Raises:
+        Exception: If the connection to the PostgreSQL database fails.
+
+    """
     try:
         connection = psycopg2.connect(url)
         cursor = connection.cursor()
@@ -46,13 +34,53 @@ def connect_postgres(url):
         print('Postgres connection failed:', e)
 
 
-    
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/get_poi')
+def get_poi():
+    # Connect to the database
+    # Execute SQL query to select test data
+    connection, cursor = connect_postgres(POSTGRES_URL)
+
+    cursor.execute("""
+                    SELECT
+                        poi_id,
+                        poi_name,
+                        poi_price,
+                        poi_lat,
+                        poi_long,
+                        poi_address,
+                        poi_suburb,
+                        poi_type_name
+                    FROM POI NATURAL JOIN POI_TYPE;
+                   """)
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+    # Close cursor and connection
+    cursor.close()
+    connection.close()
+    result = [{'id': row[0], 
+                'name': row[1],
+                'price': row[2], 
+                'latitude': row[3], 
+                'longitude': row[4], 
+                'address': row[5], 
+                'suburb': row[6],
+                'activity_type': row[7],
+                } for row in rows]
+    # Return JSON response
+    return jsonify(result)
+
+
+
+app.run()
+if __name__ == '__main__':
+    # port = int(os.getenv('PORT'))
+    # print(port)
+    # app.run(host="0.0.0.0", port=port)
 
     app.run()
-if __name__ == '__main__':
-    port = int(os.getenv('PORT'))
-    print(port)
-    app.run(host="0.0.0.0", port=port)
-
-    # app.run()
     connection, cursor = connect_postgres(POSTGRES_URL)
