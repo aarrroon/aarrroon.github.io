64d6bf2d2bdffc0e8ff6dd8149b0b4aa454cc205
Aaron Tran <aarrron.tran@gmail.com>
2024-03-11 16:50:44 +1100

    Bug Fix

--- backend/app.py
+++ backend/app.py
@@ -78,7 +78,7 @@ def get_poi():
 
 app.run()
 if __name__ == '__main__':
-    port = int(os.getenv('PORT'), 5432)
+    port = int(os.getenv('PORT'), 5000)
     print(port)
     app.run(host="0.0.0.0", port=port)
 
